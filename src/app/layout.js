42e9960a2803cb9b1807b12c5cc03df0b0d7a606
Alina Karpenko <vt222_kas@student.ztu.edu.ua>
2025-05-18 19:17:57 +0300

    add speednsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/header"
 import Footer from "./components/Footer/footer"
 import './globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         <main>{children}</main>
+         <SpeedInsights />
         <Footer />
       </body>
     </html>
